9202deb97756a9fa959a6eeeb23a1b77dfe64f8b
Fildadubcek <dubcekf@gmail.com>
2021-09-10 14:26:39 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <form action="" method="post">
+  <label for="castka">Částka</label>
+
+  <input type="number" id="castka" name="castka" min="1" max="500">  
+<br>
+<br>
+<br>
+<label for ="prevod">Z čeho chcete převádět?</label>
+
+<label for="czk">CZK-EUR</label>
+
+<input type="radio" id="czk" name="money" value="CZK-EUR">
+
+<label for="eur">EUR-CZK</label>
+
+<input type="radio" id="eur" name="money" value="EUR-CZK">
+<br>
+<br>
+<input type="submit" name="Převod" id="Převod" value="Převod">
+
+</form>
+
+
+
+<label for
+
+
+
+          
+
+
+
+
+
+ </form>   
+</body>
+</html>
